4552abb9f030b174bb6689b9b9b328973d1aae1c
Adler Martoia Ribeiro <adlermribeiro@gmail.com>
2023-07-17 10:57:29 -0300

    Inclusão e exemplo do arquivo de cores

---
+++ frontend/front_vanFacil/assets/cores.js
@@ -0,0 +1,13 @@
+const cores = {
+   preto: '#000000',
+   azulProfundo: '#0d3956',
+   azul: '#1a829a',
+   gelo: '#caecf6',
+   branco: '#ffffff',
+   cinza: '#798693',
+   cinzaClaro: '#f0f8ff',
+   cinzaProfundo: '#2f333e',
+   cinzaEscuro: '#1d1f20',
+};
+
+export default cores;
