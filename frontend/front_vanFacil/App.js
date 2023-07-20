442d96808ffa9cb4fdec50d4c5719746e22a7b1c
Adler Martoia Ribeiro <adlermribeiro@gmail.com>
2023-07-20 14:27:46 -0300

    Criação da Barra de Menu do topo da tela

--- frontend/front_vanFacil/App.js
+++ frontend/front_vanFacil/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StatusBar, StyleSheet, SafeAreaView } from 'react-native';
 import M_Inicial from './src/screens/Motorista/Inicial';
+import MenuBar from './src/screens/Shared/MenuBar';
 
 export default function App() {
    return (
       <SafeAreaView style={styles.container}>
          <StatusBar style="auto" />
+         <MenuBar nomeTela={'Minhas Rotas'} />
          <M_Inicial />
       </SafeAreaView>
    );
@@ -16,6 +18,6 @@ const styles = StyleSheet.create({
       flex: 1,
       backgroundColor: '#ffffff',
       alignItems: 'center',
-      justifyContent: 'center',
+      //justifyContent: 'center',
    },
 });
