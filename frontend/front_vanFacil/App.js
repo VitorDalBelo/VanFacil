7f2781cd41173a07c72dc43fa59da0dd23e42f54
Adler Martoia Ribeiro <adlermribeiro@gmail.com>
2023-07-26 12:23:55 -0300

    Adição da fonte Rubik e instalação da splashscreen

--- frontend/front_vanFacil/App.js
+++ frontend/front_vanFacil/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { StatusBar, StyleSheet, SafeAreaView } from 'react-native';
+
+import * as SplashScreen from 'expo-splash-screen';
+
+import MenuBar from './src/screens/Shared/MenuBar';
 import M_Inicial from './src/screens/Motorista/Inicial';
 import P_Inicial from './src/screens/Passageiro/Inicial';
-import MenuBar from './src/screens/Shared/MenuBar';
+import M_Rota from './src/screens/Motorista/Rota';
+
+import {
+   useFonts,
+   Rubik_300Light,
+   Rubik_400Regular,
+   Rubik_500Medium,
+   Rubik_700Bold,
+} from '@expo-google-fonts/rubik';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+   const [fontsLoaded] = useFonts({
+      RubikLight: Rubik_300Light,
+      RubikRegular: Rubik_400Regular,
+      RubikMedium: Rubik_500Medium,
+      RubikBold: Rubik_700Bold,
+   });
+
+   const onLayoutRootView = useCallback(async () => {
+      if (fontsLoaded) {
+         await SplashScreen.hideAsync();
+      }
+   }, [fontsLoaded]);
+
+   if (!fontsLoaded) {
+      return null;
+   }
+
    return (
-      <SafeAreaView style={styles.container}>
+      <SafeAreaView style={estilos.container} onLayout={onLayoutRootView}>
          <StatusBar style="auto" />
          <MenuBar nomeTela={'Minhas Rotas'} />
-         <P_Inicial />
+         <M_Rota />
       </SafeAreaView>
    );
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
    container: {
       flex: 1,
       backgroundColor: '#ffffff',
       alignItems: 'center',
-      //justifyContent: 'center',
    },
 });
