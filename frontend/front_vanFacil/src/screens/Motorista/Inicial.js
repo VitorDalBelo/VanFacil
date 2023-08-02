63889223b9130d8f3c51cf5925260d03e1082e84
hendgarcia <81039637+hendgarcia@users.noreply.github.com>
2023-08-02 16:56:21 -0300

    Update Inicial.js

--- frontend/front_vanFacil/src/screens/Motorista/Inicial.js
+++ frontend/front_vanFacil/src/screens/Motorista/Inicial.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList, StyleSheet,SafeAreaView ,StatusBar} from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 
 import cores from '../../../assets/cores';
 
@@ -8,20 +8,12 @@ import BtnNovaRota from '../Shared/Inicial/BtnNovaRota';
 
 import rotas from '../../mocks/rotas';
 
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import MenuBar from '../Shared/MenuBar';
-
-
 export default function Inicial() {
    return (
-      <GestureHandlerRootView style={{ flex: 1 }}>
-          <SafeAreaView style={estilos.container} onLayout={onLayoutRootView}>
-             <StatusBar style="auto" />
-             <MenuBar nomeTela={'USCS - Noite'} />
-             <ListaRotas />
-            <BtnNovaRota />
-          </SafeAreaView>
-       </GestureHandlerRootView>
+      <>
+         <ListaRotas />
+         <BtnNovaRota />
+      </>
    );
 }
 function ListaRotas() {
@@ -35,12 +27,3 @@ function ListaRotas() {
       />
    );
 }
-
-
-const estilos = StyleSheet.create({
-   container: {
-      flex: 1,
-      backgroundColor: '#ffffff',
-      alignItems: 'center',
-   },
-});
