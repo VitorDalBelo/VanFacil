3275a2ca30d5567898ae21406032ce48107924af
hendgarcia <81039637+hendgarcia@users.noreply.github.com>
2023-08-08 15:56:12 -0300

    mapa

--- frontend/front_vanFacil/src/routes/AppRotas.js
+++ frontend/front_vanFacil/src/routes/AppRotas.js
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import M_Inicial from '../screens/Motorista/Inicial';
 import M_RotaAtiva from '../screens/Motorista/RotaAtiva';
 import M_rota from '../screens/Motorista/Rota';
+import mapa from '../screens/Shared/Rota/mapa_teste'
 
 const Stack = createNativeStackNavigator();
 
@@ -12,7 +13,8 @@ export default function AppRotas() {
    return (
       <NavigationContainer>
          <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="M_inicial" component={M_Inicial} />
+            <Stack.Screen name="M_inicial" component={mapa} />
+            {/* <Stack.Screen name="M_inicial" component={M_Inicial} /> */}
             <Stack.Screen name="M_RotaAtiva" component={M_RotaAtiva} />
             <Stack.Screen name="M_Rota" component={M_rota} />
          </Stack.Navigator>
