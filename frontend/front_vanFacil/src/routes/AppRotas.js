dff5d64457df31ae7b51f9af297faffb0bdcff77
hendgarcia <81039637+hendgarcia@users.noreply.github.com>
2023-09-07 10:50:57 -0300

    cadastro

--- frontend/front_vanFacil/src/routes/AppRotas.js
+++ frontend/front_vanFacil/src/routes/AppRotas.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Navegação from '../screens/testeNavegação';
+import Cadastro from '../screens/cadastro'
+
 import M_Inicial from '../screens/Motorista/Inicial';
 import M_RotaAtiva from '../screens/Motorista/RotaAtiva';
 import M_Rota from '../screens/Motorista/Rota';
@@ -19,6 +21,7 @@ export default function AppRotas() {
    return (
       <NavigationContainer>
          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Cadastro" component={Cadastro} />
             <Stack.Screen name="Navegação" component={Navegação} />
 
             <Stack.Screen name="M_Inicial" component={M_Inicial} />
