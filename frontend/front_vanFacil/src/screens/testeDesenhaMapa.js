777e3936b1f7983b271fdec7eef7392fd6d840c4
Adler Martoia Ribeiro <adlermribeiro@gmail.com>
2023-09-19 16:57:00 -0300

    Adição da confirmação do apagar desenho

--- frontend/front_vanFacil/src/screens/testeDesenhaMapa.js
+++ frontend/front_vanFacil/src/screens/testeDesenhaMapa.js
@@ -1,10 +1,11 @@
 import React, { useReducer, useState } from 'react';
-import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { Modal, StyleSheet, TouchableOpacity, View } from 'react-native';
 import MapView, { Marker, Polygon } from 'react-native-maps';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, Feather } from '@expo/vector-icons';
 
 import cores from '../../assets/cores';
 import Texto from '../components/Texto';
+import MenuBar from '../screens/Shared/MenuBar';
 
 export default function TesteDesenhaMapa() {
    const [mexerMapa, setMexerMapa] = useReducer((mexerMapa) => !mexerMapa, true);
@@ -33,8 +34,24 @@ export default function TesteDesenhaMapa() {
       setMarcadores(marcadores.slice(0, -1));
    };
 
+   const [modalVisible, setModalVisible] = useState(false);
+
+   const mostrarModal = () => {
+      setModalVisible(true);
+   };
+
+   const confirmar = () => {
+      limpaMarcadores();
+      setModalVisible(false);
+   };
+
+   const cancelar = () => {
+      setModalVisible(false);
+   };
+
    return (
       <>
+         <MenuBar nomeTela={'Área de atuação'} />
          <MapView
             style={estilos.mapa}
             rotateEnabled={mexerMapa}
@@ -66,22 +83,43 @@ export default function TesteDesenhaMapa() {
                <TouchableOpacity style={[estilos.botao, mexerMapa ? {} : { backgroundColor: cores.azul }]} onPress={setMexerMapa}>
                   <Ionicons name="color-palette" size={24} style={estilos.img} />
                </TouchableOpacity>
+
                <TouchableOpacity
                   style={[estilos.botao, desabilitaEdicao ? { backgroundColor: 'gray' } : {}]}
-                  onPress={limpaMarcadores}
+                  onPress={mostrarModal}
                   disabled={desabilitaEdicao}
                >
-                  <Ionicons name="trash-bin" size={24} style={estilos.img} />
+                  <Feather name="trash-2" size={24} style={estilos.img} />
                </TouchableOpacity>
+
                <TouchableOpacity
                   style={[estilos.botao, desabilitaEdicao ? { backgroundColor: 'gray' } : {}]}
                   onPress={desfazUltimo}
                   disabled={desabilitaEdicao}
                >
-                  <Ionicons name="arrow-undo-outline" size={24} style={estilos.img} />
+                  <Ionicons name="arrow-undo" size={24} style={estilos.img} />
                </TouchableOpacity>
             </View>
          </View>
+
+         <Modal animationType="fade" transparent={true} visible={modalVisible} onRequestClose={cancelar}>
+            <View style={modal.overlay}>
+               <View style={modal.caixaCentral}>
+                  <View style={modal.titleContainer}>
+                     <Texto style={modal.titulo}>Confirmação</Texto>
+                  </View>
+                  <Texto>Deseja apagar o desenho e os pontos?</Texto>
+                  <View style={modal.linhaBotoes}>
+                     <TouchableOpacity style={[modal.button, modal.btnConfirmar]} onPress={confirmar}>
+                        <Texto style={modal.buttonText}>Confirmar</Texto>
+                     </TouchableOpacity>
+                     <TouchableOpacity style={[modal.button, modal.btnCancelar]} onPress={cancelar}>
+                        <Texto style={modal.buttonText}>Cancelar</Texto>
+                     </TouchableOpacity>
+                  </View>
+               </View>
+            </View>
+         </Modal>
       </>
    );
 }
@@ -112,5 +150,70 @@ const estilos = StyleSheet.create({
    },
    img: {
       color: cores.branco,
+   },
+});
+
+const modal = StyleSheet.create({
+   overlay: {
+      flex: 1,
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      justifyContent: 'center',
+      alignItems: 'center',
+   },
+   caixaCentral: {
+      width: 310,
+      backgroundColor: cores.branco,
+      borderRadius: 10,
+      alignItems: 'center',
+      borderWidth: 1,
+      borderColor: cores.cinzaClaro,
+   },
+   titleContainer: {
+      alignItems: 'center',
+      backgroundColor: cores.cinzaClaro,
+      padding: 10,
+      borderTopLeftRadius: 9,
+      borderTopRightRadius: 9,
+      width: '100%',
+      marginBottom: 10,
+   },
+   titulo: {
+      fontSize: 22,
+      fontWeight: 'bold',
+   },
+   texto: {
+      fontSize: 16,
+      marginBottom: 20,
+   },
+   textoPrincipal: {
+      fontSize: 16,
+      marginBottom: 20,
+      fontWeight: 'bold',
+   },
+   linhaBotoes: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+   },
+   button: {
+      flex: 1,
+      alignItems: 'center',
+      paddingVertical: 10,
+      paddingHorizontal: 15,
+      borderRadius: 5,
+      margin: 10,
+   },
+   buttonText: {
+      color: cores.branco,
+      fontWeight: 'bold',
+   },
+   btnConfirmar: {
+      backgroundColor: cores.azul,
+      marginRight: 10,
+      alignSelf: 'flex-start',
+   },
+   btnCancelar: {
+      backgroundColor: cores.vermelho,
+      marginLeft: 10,
+      alignSelf: 'flex-end',
    },
 });
